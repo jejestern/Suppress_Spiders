3bfb97efe70484a9bbda6d0acb992bd3991be46e
Jennifer <studerje@ethz.ch>
2021-11-12 14:39:29 +0100

    Trying FFT masks for R150-300 and R300-450

--- Programs/fft.py
+++ Programs/fft.py
@@ -9,10 +9,10 @@ Created on Tue Oct 19 11:44:30 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("/home/jeje/Dokumente/Masterthesis/Programs/rphi_plane_spline3_R150_R300.txt").reshape(150, 1413)
+data = np.loadtxt("/home/jeje/Dokumente/Masterthesis/Programs/rphi_plane_spline3_R150_R300.txt").reshape(150, 2356)
 
 fig, ax = plt.subplots(1,1)
-im = ax.imshow(data, origin='lower', aspect='auto', vmin=0, vmax= 20, 
+im = ax.imshow(data, origin='lower', aspect='auto', vmin=0, vmax= 5, 
                extent=[0, 360, 150, 300])
 plt.tight_layout()
 plt.colorbar(im)
@@ -26,12 +26,28 @@ plt.tight_layout()
 plt.colorbar()
 plt.show()
 
-#fourier[-70: , 700:710] = 1
-#fourier[:70, 700:710] = 1
+# R150-300 usual length
+#fourier[65:85, -706:-550] = 1
+#fourier[65:85, 550:706] = 1
 
-fourier[65:85, -700:] = 1
-fourier[65:85, :700] = 1
+# R150-300 1000x1000
+#fourier[400:600, -600:600] = 1
 
+# R300-450 usual length
+fourier[65:85, -1177:-800] = 1
+fourier[65:85, 800:1178] = 1
+
+"""
+x_len, y_len = fourier.shape
+x_center = x_len/2 - 1
+y_center = y_len/2 - 1
+# Define the corresponding polar coordinates to the x-y coordinates
+r_array, phi_array = polar_corrdinates_grid((x_len, y_len), (x_center, y_center))
+mask_r = (r_array <= 50)
+mask_2 = (r_array >= 60)
+
+fourier = fourier*(mask_2 + mask_r)
+"""
 plt.figure()
 plt.imshow(np.log(abs(fourier)), cmap='gray', aspect='auto')
 plt.tight_layout()
@@ -40,7 +56,7 @@ plt.show()
 
 img_back = abs(np.fft.ifft2(fourier))
 fig, ax = plt.subplots(1,1)
-im = ax.imshow(img_back, origin='lower', aspect='auto', vmin=0, vmax= 20, 
+im = ax.imshow(img_back, origin='lower', aspect='auto', vmin=0, vmax= 5, 
                extent=[0, 360, 150, 300])
 plt.tight_layout()
 plt.colorbar(im)
