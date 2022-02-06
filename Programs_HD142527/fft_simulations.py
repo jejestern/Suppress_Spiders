91f19beb21f3fac8eea1b42caedd90be5f6639b6
Jennifer <studerje@ethz.ch>
2022-02-06 01:55:13 +0100

    Polish up the plots for fft horizontal and vertical beam section in report.

--- Programs_HD142527/fft_simulations.py
+++ Programs_HD142527/fft_simulations.py
@@ -8,11 +8,16 @@ Created on Tue Nov 23 12:21:19 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
 
 def fourier(image, title):
     
     fourier_im = np.fft.fftshift(np.fft.fft2(image))
+
+    # Frequency plane
+    freq = np.fft.fftfreq(image.shape[0])
+    freq = np.fft.fftshift(freq)
 
     # Plot the output
     plt.figure(figsize=(8,3.5))
@@ -20,17 +25,33 @@ def fourier(image, title):
     plt.subplot(121)
     plt.imshow(image, origin='lower', cmap='gray', vmin=0, vmax=1.0)
     plt.title(r'Image')
+    plt.xlabel(r'x')
+    plt.ylabel(r'y')
     plt.colorbar()
     
     epsilon = 10**(-6) # In order to be able to take the log
     
     plt.subplot(122)
-    plt.imshow(np.log(abs(fourier_im)+epsilon), origin='lower', cmap='gray', vmin=-1, vmax=5.0)
+    plt.imshow(abs(fourier_im+epsilon), origin='lower', cmap='gray', norm=LogNorm(vmin=1),
+               extent=[freq[0], freq[-1], freq[0], freq[-1]])
     plt.title(r'Fourier Transformed Image')
+    plt.xlabel(r'Frequency [$\frac{1}{\mathrm{px}}$]')
+    plt.ylabel(r'Frequency [$\frac{1}{\mathrm{px}}$]')
     plt.colorbar()
         
     plt.tight_layout()
     plt.savefig("fourier/fft_simulation"+ title +".pdf")
+    plt.show()
+    
+    # Plot the horizontal cut thorugh the FFT
+    plt.figure()
+    plt.semilogy(freq, abs(fourier_im[int(image.shape[0]/2), :] + 0.0001))
+    #plt.ylim((10**(0), 10**(3)))
+    plt.title("FFT horizontal cut at freq 0")
+    #plt.xlabel(r'Angular frequency [$\frac{1}{\mathrm{rad}}$]')
+    #plt.legend()
+    plt.tight_layout()
+    plt.savefig("fourier/fft_simulation_cut"+ title +".pdf")
     plt.show()
     
     return fourier_im
